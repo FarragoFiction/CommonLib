5f501c02718c0e6d19f1167dbb490ef437120d1b
Unknown <emperorlexx@hotmail.com>
2018-05-19 05:39:40 +0100

    Actually export collection_utils

--- lib/Utility.dart
+++ lib/Utility.dart
@@ -1,3 +1,4 @@
+export "package:CommonLib/src/utility/collection_utils.dart";
 export "package:CommonLib/src/utility/math_utils.dart";
 export "package:CommonLib/src/utility/path_utils.dart";
 export "package:CommonLib/src/utility/predicates.dart";
