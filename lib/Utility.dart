e839489cdee40bd33f595984affc3cc6a8daadee
jadedResearcher <gtg287y@gmail.com>
2018-07-25 10:30:45 -0400

    added the json wrapper convinience methods I use for serializing data

--- lib/Utility.dart
+++ lib/Utility.dart
@@ -3,3 +3,4 @@ export "package:CommonLib/src/utility/math_utils.dart";
 export "package:CommonLib/src/utility/path_utils.dart";
 export "package:CommonLib/src/utility/predicates.dart";
 export "package:CommonLib/src/utility/text_search.dart";
+export "package:CommonLib/src/utility/JSONObject.dart";
