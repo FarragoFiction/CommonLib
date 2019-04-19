b4b9c88c73bb3567b53ed51a09983d67cfcc2b47
TTFTCUTS <emperorlexx@hotmail.com>
2019-04-19 17:56:10 +0100

    test updates

--- test/jsonhandler_test.dart
+++ test/jsonhandler_test.dart
@@ -2,30 +2,34 @@
 
 import 'package:CommonLib/src/utility/jsonhandler.dart';
 import 'package:test/test.dart';
+
 void main() {
-    print("is literally anything ahppening?");
-    //List<int> seadwellerBodies = dataList<int>("Lamia.seaDwellerBodies");
-    test("String.split() splits the string on the delimiter", () {
-        var string = "foo,bar,baz";
-        expect(string.split(","), equals(["foo", "bWRONGar", "baz"]));
-    });
-
-    JsonHandler jsonHandler = simulatedDollData();
-    List<int> seadwellerBodies = jsonHandler.getArray<int>("Lamia.seaDwellerBodies");
-
-    test("Lamia seadweller bodies ", ()
+    group("is literally anything happening?", ()
     {
-        expect(seadwellerBodies, equals( <int>[9999999,7,8,9,12,13,27,28,29,34,35,39,40,46,50,51,52,60,61]));
-    });
+        JsonHandler jsonHandler;
+        List<int> seadwellerBodies;
 
+        setUp(() {
+            jsonHandler = simulatedDollData();
+            seadwellerBodies = jsonHandler.getArray<int>("Lamia.seaDwellerBodies");
+        });
+
+        test("String.split() splits the string on the delimiter", () {
+            const String string = "foo.bar.baz";
+            expect(string.split("."), equals(<String>["foo", "bWRONGar", "baz"]));
+        });
+
+        test("Lamia seadweller bodies ", () {
+            expect(seadwellerBodies, equals(<int>[9999999, 7, 8, 9, 12, 13, 27, 28, 29, 34, 35, 39, 40, 46, 50, 51, 52, 60, 61]));
+        });
+    });
 }
 
 
 JsonHandler simulatedDollData() {
-    Map<String,dynamic> json = new Map<String,dynamic>();
-    json["Lamia"] = new Map<String, dynamic>();
+    Map<String,dynamic> json = <String,dynamic>{};
+    json["Lamia"] = <String, dynamic>{};
     json["Lamia"]["seaDwellerBodies"] = <int>[7,8,9,12,13,27,28,29,34,35,39,40,46,50,51,52,60,61];
     json["Lamia"]["layers"] = {"Body": 77};
     return new JsonHandler(json);
-
 }
